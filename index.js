b475fd2ba526743a21404da20884cbc6a27eba21
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-07 13:07:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 const currentUser = 'Grace Hopper';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
